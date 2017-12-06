f2876cb43c08f00f5a0811623c184a22b28a3921
ronnie1734 <rlewwwis@yahoo.com>
2017-12-05 21:31:59 -0800

    started category filter

--- trevor-ronnie/scripts/articleView.js
+++ trevor-ronnie/scripts/articleView.js
@@ -55,11 +55,20 @@ articleView.handleAuthorFilter = function() {
 };
 
 articleView.handleCategoryFilter = function() {
-  // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
-  // When an option with a value is selected, hide all the articles, then reveal the matches.
-  // When the blank (default) option is selected, show all the articles, except for the template.
-  // Be sure to reset the #author-filter while you are at it!
+  $('#author-filter').on('change', function() {
+    if ($(this).val()) {
+      var category = $(this).val();
+      $('article').hide();
+      $('article[data-category="' + category + '"]').show();
+    // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
+    // When an option with a value is selected, hide all the articles, then reveal the matches.
+    // When the blank (default) option is selected, show all the articles, except for the template.
+    // Be sure to reset the #author-filter while you are at it!
+    } else{
 
+  }
+$('#category-filter').val('');
+});
 };
 
 articleView.handleMainNav = function() {
